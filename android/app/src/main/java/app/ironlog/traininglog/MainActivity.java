772b76dbb2210b88776a64b8b08d50977b9fdeb4
okasei140926-spec <okasei.14.0926@gmail.com>
2026-05-03 15:15:43 +0900

    update

---
+++ android/app/src/main/java/app/ironlog/traininglog/MainActivity.java
@@ -0,0 +1,5 @@
+package app.ironlog.traininglog;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "app.ironlog.traininglog",
+  appName: "IRON LOG",
+  webDir: "build",
+  bundledWebRuntime: false,
+};
+
+export default config;
